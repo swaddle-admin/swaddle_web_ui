8bc19328b7988d965375112cd173cb7329e256ae
Sharon Wigwe <sharonwigwe@gmail.com>
2026-05-15 23:54:58 +0100

    feat(layout): add App shell and ChatPage scaffold

---
+++ src/pages/ChatPage.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "@mantine/core";
+
+const ChatPage = () => {
+  return (
+    <Stack h="100vh" justify="space-between">
+      <div>Chat area goes here</div>
+      <div>Input goes here</div>
+    </Stack>
+  );
+};
+
+export default ChatPage;
